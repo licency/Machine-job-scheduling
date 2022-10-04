75d3ff76d5e7ea215483a12f08d56d8e219b171e
licency <69596770+licency@users.noreply.github.com>
2022-10-04 15:08:23 +0530

    Create displayList.c

---
+++ CUT/Code/src/displayList.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void displayList(machine *list)
+{
+  printf("\n\n~~~~~Contents of Linked list~~~~~\n");
+  job *temp = NULL;
+  
+  for(temp = list->head; temp != list->tail->next; temp = temp->next)
+  {
+    printf("%s\t%s\t%s\t%d\t%s\n\n", temp->jno, temp->jd, temp->m, temp->time, temp->client);
+  }
+}
