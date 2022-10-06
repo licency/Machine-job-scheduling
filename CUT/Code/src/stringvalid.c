4f860f6bdb4d5a21a386ae13fb194a63678976c8
licency <69596770+licency@users.noreply.github.com>
2022-10-06 20:47:41 +0530

    Update stringvalid.c

--- CUT/Code/src/stringvalid.c
+++ CUT/Code/src/stringvalid.c
@@ -8,28 +8,27 @@ int stringValid(char str[MAX])
 	int c=0;
 	if(str[0]=='J' || str[0]=='j')
 	{
-	while(str[k])
-	{
-		if(str[k]==':')
+		while(str[k])
 		{
-			c++;
-			if(c==2)
+			if(str[k]==':')
 			{
-				if(!(str[k+1]=='M' || str[k+1]=='m'))
-					break;
-						else if(str[k+1]=='M' || str[k+1]=='m')
+				c++;
+				if(c==2)
+				{
+					if(!(str[k+1]=='M' || str[k+1]=='m'))
+						break;
+					else if(str[k+1]=='M' || str[k+1]=='m')
+					{
+						if(!(str[k+2]>='1' && str[k+2]<='3'))
 						{
-							if(!(str[k+2]>='1' && str[k+2]<='3'))
-							{
-								break;
-							}
+							break;
 						}
-			}
-			if(c==3)
-			{
-				if(!(str[k+1]>='0' && str[k+1]<='9'))
-					break;
-
+					}
+				}
+				if(c==3)
+				{
+					if(!(str[k+1]>='0' && str[k+1]<='9'))
+						break;
 					if(str[k+1]>='0' && str[k+1]<='9')
 					{
 						if(!(str[k+2]>='0' && str[k+2]<='9'))
@@ -37,18 +36,19 @@ int stringValid(char str[MAX])
 							break;
 						}
 					}
+				}
 			}
+			k++;
 		}
-		k++;
-	}
-	if(c>=3)
-	{
-		return SUCC;
-	}
-	else
-	{
-		return FAIL;
-	}
+		if(c>=3)
+		{
+			return SUCC;
+		}
+		else
+		{
+			return FAIL;
+		}
 							     
 	}
+	else return FAIL; //removing warning
 }
